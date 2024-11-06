19c596ff8a4110df28324acd366413db062f403c
TuNombre <tuemail@example.com>
2024-11-06 19:08:02 -0300

    Primer cambio ejercicio

---
+++ ejercicio2.php
@@ -0,0 +1,11 @@
+<?php
+$entero = 10;        // Tipo integer
+$doble = 3.14;       // Tipo double
+$cadena = "Hola";    // Tipo string
+$booleano = true;    // Tipo boolean
+
+echo "Entero: $entero<br>";
+echo "Double: $doble<br>";
+echo "String: $cadena<br>";
+echo "Booleano: " . ($booleano ? 'true' : 'false') . "<br>";
+?>
